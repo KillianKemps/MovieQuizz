1ff7e1814b9c8294e6e69a3e554c54fb825b1761
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-15 15:00:27 +0100

    Remove useless logs

--- scripts/views/quizz-view.js
+++ scripts/views/quizz-view.js
@@ -10,23 +10,19 @@ var QuizzListView = Backbone.View.extend({
     var self = this;
     this.myQuizzCollection.fetch({
       success: function(data) {
-        console.log(data);
         self.render();
       },
       error: function(error) {
         console.log('error: ', error);
       }
     });
-    console.log('$$$$$$$');
   },
 
   render: function() {
     var $renderContainer = this.$('.question-area');
-    console.log($renderContainer);
 
     $renderContainer.html(
       this.templateHandlebars(this.myQuizzCollection.toJSON())
     );
-    console.log(this.myQuizzCollection.toJSON());
   }
 });
