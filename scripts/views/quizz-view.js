aaa8f64c4ce06e67dc078cb417e085499fb4504d
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-01-19 17:59:18 +0100

    Add random on quizz list

--- scripts/views/quizz-view.js
+++ scripts/views/quizz-view.js
@@ -11,6 +11,11 @@ var QuizzListView = Backbone.View.extend({
     return this;
   },
 
+  shuffleArray: function (o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  },
+
   initialize: function() {
     console.log('Initialize in quizz view');
     this.myQuizzCollection = new QuizzCollection();
@@ -19,17 +24,20 @@ var QuizzListView = Backbone.View.extend({
 
     this.myQuizzCollection.fetch({
       success: function(data) {
+        // Shuffle the data and store it
+        self.quizzList = self.shuffleArray(self.myQuizzCollection.toJSON());
         self.render();
       },
       error: function(error) {
         console.log('Error while fetching quizz: ', error);
       }
     });
+
   },
 
   render: function() {
     this.$el.html(
-      this.templateHandlebars(this.myQuizzCollection.toJSON())
+      this.templateHandlebars(this.quizzList)
     );
   }
 });
